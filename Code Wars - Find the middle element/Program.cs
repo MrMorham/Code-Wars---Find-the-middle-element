9a10326b54ed06d71fbeeb8fbb36aca5d00a4dc6
MrMorham <g7058190@gmail.com>
2018-10-07 00:39:58 +0100

    ..

---
+++ Code Wars - Find the middle element/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Wars___Find_the_middle_element
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            double[] test1 = { 2, 1, 3 };
+            double[] test2 = { 14, 15, 10 };
+
+            double[] input;
+
+        }
+    }
+}
